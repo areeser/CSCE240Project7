cfff9d1f1a281a600037d54c2a75998d1c7169ff
Max Bozzi <mjb@mbozzi.com>
2016-11-17 00:53:31 -0500

    Gets this code to compile and run.
    See the instruction in compile-me-hack.include.cpp for how to get the
    code to compile and run.  The added file "fixes" the issue with the
    missing definition of Utils::Output by abusing the C preprocessor.
    
    Assuming that Utils::Output prints its' arguments to the screen, this
    will dump information to the log file for inspection.

---
+++ compile-me-hack.include.h
@@ -0,0 +1,45 @@
+/*     THIS FILE SHOULD NOT BE INCLUDED AS PART OF A RELEASE OR SUBMISSION.    */
+
+/* -- What does it do?
+ * Allows this program to compile and run in order to help us understand it.
+ * Calls to Output() will either
+ *    -- fail to compile with an inscrutable error message, or
+ *    -- print a string to both output and log streams, along with a tag
+ *       identifying why the output showed up.
+ *
+ * -- What is the signature:  void(string, output stream, log stream).
+ *
+ * -- How do you use this?
+ * Add the text, verbatim:
+      -include compile-me-hack.include.h
+ * at the end of the very first line of 'makefile', such that it looks like
+      GPP = g++ -O3 -Wall -std=c++11 -include compile-me-hack.include.h
+ */
+
+# if ! defined CSCE_240_PROJECT_7_HEADER_COMPILE_ME_HACK_H
+# define CSCE_240_PROJECT_7_HEADER_COMPILE_ME_HACK_H
+////////////////////////////////////////////////////////////////////////////////
+// TODO:  FIXME:
+// A nasty and error-prone hack to get this program to compile, silently, w/o
+// the definition of Utils::Output.
+int __attribute__ ((weak)) nil() { return (int)0; }
+int __attribute__ ((weak)) extern ignore;
+/* Because the macro is is inteneded to be substituted as only part of an entire
+   statement, it's not possible to resolve the name or supress warnings for just
+   the macro.  I cannot insert _Pragma() in the middle of the statement because
+   there is no way to enter blank lines whitespace in the generated code. */
+#   if defined __GNUC__
+#   pragma message "-Wunused-value off: compile-me-hack.include.h included."
+#   pragma GCC diagnostic ignored "-Wunused-value"
+#   pragma GCC diagnostic push
+#
+#   define Output(str, out, log)                                    \
+    log_stream; ignore = ::nil();                                   \
+      do {                                                          \
+        log << "Utils/output macro hack called: " << str << "\n";   \
+      } while (false)
+#
+#   endif
+////////////////////////////////////////////////////////////////////////////////
+
+# endif
