30c6d58092ad2bef99e1cdeaef79104a0a99e557
Max Bozzi <mjb@mbozzi.com>
2016-11-30 21:50:56 -0500

    Fixup the instructions in this file

--- compile-me-hack.include.h
+++ compile-me-hack.include.h
@@ -2,8 +2,8 @@
        IF USED, IT WILL CAUSE PROBLEMS WHEN THE NEEDED CODE IS AVAILABLE.     */
 
 /* -- What does it do?
- * Allows this program to compile and run in order to help us understand it.
- * Calls to Output() will either
+ *   Allows this program to compile and run in order to help us understand it.
+ *   After following the instructions below, calls to Output() will either
  *    -- fail to compile with an inscrutable error message, or
  *    -- print a string to the log streams passed to it, along with a tag
  *       identifying why the output showed up.
@@ -15,6 +15,7 @@
       -include compile-me-hack.include.h
  * at the end of the very first line of 'makefile', such that it looks like
       GPP = g++ -O3 -Wall -std=c++11 -include compile-me-hack.include.h
+ * then run make; the code should compile, with plenty of warnings.
  */
 
 # if ! defined CSCE_240_PROJECT_7_HEADER_COMPILE_ME_HACK_H
@@ -28,7 +29,7 @@ int __attribute__ ((weak)) extern ignore;
 /* Because the macro is is intended to be substituted as only part of an entire
    statement, it's not possible to resolve the name or suppress warnings for just
    the macro.  I cannot insert _Pragma() in the middle of the statement because
-   there is no sane way to enter blank lines before the generated code. */
+   there is no way to enter blank lines before the generated code. */
 #   if defined __GNUC__
 #
 #   pragma message "-Wunused-value off: compile-me-hack.include.h included"
@@ -43,5 +44,4 @@ int __attribute__ ((weak)) extern ignore;
 #
 #   endif
 ////////////////////////////////////////////////////////////////////////////////
-
 # endif
