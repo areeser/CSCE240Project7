7716a2db2a54957804483071b0de494370b7e139
regalbutoj <jfr@email.sc.edu>
2016-12-01 17:59:10 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
  * a polling simulation for voter precincts. 
  *
  * Author/copyright:  Duncan Buell. All rights reserved.
- * Modified by: Group 7
+ * Modified by: Project 7 group
  * Date: 1 December 2016
  *
 **/
