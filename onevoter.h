a4c309fa771f8fae70d7d594a37580d34fc41b08
regalbutoj <jfr@email.sc.edu>
2016-12-01 11:24:59 -0500

    comments onevoter.h

--- onevoter.h
+++ onevoter.h
@@ -1,8 +1,17 @@
 /****************************************************************
- * Header for the 'OneVoter' class
+ * Header for the 'OneVoter' class.
+ * A voter has a sequence, arrival time, and time spent voting as
+ * parameters in the constructor. It also contains accessors for
+ * various private variables.
+ * 
+ * General functions assign a voter station and format the
+ * output for the user, the details for a voter. Voters are
+ * are created until the simulation stops it and each voter is
+ * a part of a voter precinct (onepct.cc). 
  *
  * Author/copyright:  Duncan Buell
- * Date: 6 October 2016
+ * Modified by Group 7
+ * Date: 1 December 2016
  *
 **/
 
@@ -22,10 +31,13 @@ public:
 **/
  OneVoter() = default;
  OneVoter(int sequence, int arrival_seconds, int duration_seconds);
+ //each voter has a unique sequence, arrival time (in seconds), 
+ //and time spent voting (duration) 
+ 
  virtual ~OneVoter() = default;
 
 /****************************************************************
- * Accessors and Mutators.
+ * Accessors for voter details 
 **/
  int GetStationNumber() const;
  int GetTimeArrival() const;
@@ -33,7 +45,11 @@ public:
  int GetTimeWaiting() const;
 
 /****************************************************************
- * General functions.
+ * General functions to assign the voter to a station and check
+ * if the voter has finished voting. Also has a ToString() and 
+ * GetTimeInQ() that returns the amount of time the voter spent
+ * waiting in line. The ToStringHeader() formats the output
+ * without starting a new line for use in a table. 
 **/
  void AssignStation(int station_number, int start_time_seconds);
  void DoneVoting();
@@ -50,9 +66,12 @@ private:
  int time_vote_duration_seconds_ = kDummyVoterInt;
  int time_waiting_seconds_ = kDummyVoterInt;
  int which_station_ = kDummyVoterInt;
+ //sets member variable values
 
 /****************************************************************
- * General private functions.
+ * General private functions to convert voter times from seconds
+ * into a readable format for the user as they will be in 1000s
+ * of seconds. These are used in the ToString().
 **/
  string ConvertTime(int time_in_seconds) const;
  string GetTOD(int time) const;
